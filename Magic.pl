2a08f8c1fa1f1d0db6973224c07216041208c16e
Kjell Håland <kjellvis@live.com>
2017-11-20 17:42:17 +0100

    Tried to implement a representation of array of arrays

--- Magic.pl
+++ Magic.pl
@@ -1,22 +1,12 @@
 :- use_module(library(clpfd)).
 
-magic(Base, Matrix, Size) :-
-    equality(Base, Matrix),
+magic(Matrix, Size) :-
+    transpose(Matrix, Transposed),
+    equality(Matrix),
     rule1(Matrix),
     rule2(Matrix),
-    transpose(Matrix, Transposed),
     rule1(Transposed),
     rule2(Transposed).
-
-
-
-% BlackRule
-
-ruleBlack([]).
-ruleBlack([H|T]) :- ruleBlackCheck(H), ruleBlack(T).
-
-ruleBlackCheck([]).
-ruleBlackCheck([E1,E2|T]) :- E1=0,E2\=0, ruleBlackCheck(L,T).
 
 % Rule 1 - Checks if there are duplicates in a row or column
 rule1([]).
@@ -24,8 +14,16 @@ rule1([H|T]) :- rule1Check(H), rule1(T).
 
 rule1Check(L) :- rule1Check(L,L).
 rule1Check(_,[]).
-rule1Check(L, [H|T]) :- H = 0, rule1Check(L,T).
-rule1Check(L, [H|T]) :- H \= 0, count(L,H,R), R=1, rule1Check(L,T).
+rule1Check(L, [ [_,S] |T]) :- S=0, rule1Check(L,T).
+rule1Check(L, [ [V,S] |T]) :- S\=0, noDuplicates(L,V), rule1Check(L,T).
+
+noDuplicates(L,V) :- noDuplicates(L,V,0). 
+noDuplicates([],_,1).
+noDuplicates([],_,0).
+noDuplicates(_,_,2) :- false.
+noDuplicates([ [V,S] |T], X, R) :- V=X, S\=0, A is R+1, noDuplicates(T,X,A).
+noDuplicates([ [V,S] |T], X, R) :- (V\=X ; S=0), noDuplicates(T,X,R).
+
 
 % Rule 2 - Checks if there are adjacent blacks in a row or col
 rule2([]).
@@ -33,30 +31,41 @@ rule2([H|T]) :- rule2Check(H), rule2(T).
 
 rule2Check([]) :- true.
 rule2Check([_]) :- true.
-rule2Check([I1,I2|T]) :- rule2Check([I2|T]), not( (I1=0, I2=0) ). 
+rule2Check([ [_,0], [_,0] |T]) :- rule2Check([_|T]), false.
+rule2Check([ [_,0], [_,1] |T]) :- rule2Check([_|T]).
+rule2Check([ [_,1], [_,0] |T]) :- rule2Check([_|T]).
+rule2Check([ [_,1], [_,1] |T]) :- rule2Check([_|T]).
 
 
 % Creates an empty matrix with the same dimensions as the given matrix
 constructMatrix([],[]).
 constructMatrix([H|T], [A|B]) :- constructRow(H,A), constructMatrix(T,B).
 constructRow([],[]).
-constructRow([A|T1],[_|T2]) :- constructRow(T1,T2).
+constructRow([A|T1],[ [A,_] |T2]) :- constructRow(T1,T2).
 
 % Equality rule
-equality([],[]).
-equality([H1|T1], [H2|T2]) :- eq(H1,H2), equality(T1,T2).
+equality([]).
+equality([H|T]) :- eq(H), equality(T).
 
-eq([],[]).
-eq([H1|T1], [H2|T2]) :- (H1=1 ; H2=0 ), eq(T1,T2).
+eq([]).
+eq([ [_,S] |T]) :- (S=1 ; S=0 ), eq(T).
 
 % Counts the occuriencies of the element X in the list.
 count([],_,0).
-count([X|T],X,Y):- count(T,X,Z), Y is 1+Z.
-count([X1|T],X,Z):- X1\=X,count(T,X,Z).
+count([ [V,_] |T], X, Z):- V\=X, count(T,X,Z).
+count([ [X,1] |T], X, Y):- count(T,X,Z), Y is 1+Z.
+count([ [_,0] |T], X, Z):- count(T,X,Z).
+
 
 
 % Problem solving things...
-solvedProblem([[0,2,5,4,3],[4,5,0,1,0],[1,0,3,0,4],[3,4,1,2,5],[0,1,4,0,2]]).
+solvedProblem([
+    [0,2,5,4,3],
+    [4,5,0,1,0],
+    [1,0,3,0,4],
+    [3,4,1,2,5],
+    [0,1,4,0,2]]).
+
 hProblem([
     [1,2,5,4,3],
     [4,5,4,1,1],
@@ -71,7 +80,10 @@ test_magic :-
     statistics(runtime, _),
     hProblem(Base),
     constructMatrix(Base, Matrix),
-    magic(Base, Matrix, 5),  
+    magic(Matrix, 5),  
     maplist(writeln, Matrix),
     statistics(runtime, [_,T]),
     write('CPU time = '), write(T), write(' msec'), nl, nl, true. 
+
+
+    % [ [1,0],[1,1],[2,1],[2,0],[1,0] ]
